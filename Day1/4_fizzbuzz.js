b24020795784944b5051e9a4e2859de40f98f8de
Alla Rakhimova <alla.rakhimova103@gmail.com>
2021-07-26 23:06:48 +0300

    FizzBuzz ready

--- Day1/4_fizzbuzz.js
+++ Day1/4_fizzbuzz.js
@@ -1,15 +1,16 @@
 function FizzBuzz(N) {
   const result = [];
   for (let i = 1; i <= N; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      result.push("FizzBuzz");
-    } else if (i % 3 === 0) {
-      result.push("Fizz");
-    } else if (i % 5 === 0) {
-      result.push("Buzz");
-    } else {
-      result.push(i);
+    let str = '';
+    
+    if (i % 3 === 0) {
+      str += "Fizz";
     }
+    if (i % 5 === 0) {
+      str += "Buzz";
+    }
+
+    result.push(str || i);
   }
 
   return result;
