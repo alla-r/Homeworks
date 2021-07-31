8d54e68bea0fadfb03ad7da59dd947dd2f11ea41
Alla Rakhimova <alla.rakhimova103@gmail.com>
2021-07-31 22:01:28 +0300

    task 2 ready

--- Day3/2_ascending.js
+++ Day3/2_ascending.js
@@ -2,7 +2,7 @@ class arrOfNumbersInAscendingOrder {
   constructor(arrToSort) {
     this.arrOfNumbers = arrToSort ? this._getSortedArray(arrToSort) : [];
     this.length = this.arrOfNumbers.length;
-  };
+  }
 
   _getSortedArray(arrToSort) {
     return arrToSort ? 
@@ -12,22 +12,17 @@ class arrOfNumbersInAscendingOrder {
 
   add(newNumber) {
     this.arrOfNumbers.push(newNumber);
-    this._getSortedArray()
-    // this.arrOfNumbers.sort((firstEl, secondEl) => firstEl - secondEl);
+    this._getSortedArray();
     this.length++;
   }
 
   get(i) {
-    return this.arrOfNumbers[i];
+    return this.arrOfNumbers[i - 1];
   }
 }
 
 const arr = new arrOfNumbersInAscendingOrder([8, 2, 11, 0, 3]);
-// arr.add(6);
-// arr.add(11);
-// arr.add(8);
-
-console.log(arr);
+arr.add(15);
 arr.add(9);
 console.log(arr);
-console.log(arr.get(2));
+console.log(arr.get(1));
